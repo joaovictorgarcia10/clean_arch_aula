e0b0a2640af28cf066fa562bfe0691d0f6dca2b8
João Victor Garcia <joao.santana@rarolabs.com.br>
2022-06-21 12:12:12 -0300

    fixes

--- lib/shared/utils/constants/app_theme.dart
+++ lib/shared/utils/constants/app_theme.dart
@@ -4,11 +4,11 @@ import 'app_colors.dart';
 class AppTheme {
   ThemeData appTheme() {
     return ThemeData(
-      useMaterial3: true,
       scaffoldBackgroundColor: AppColors.background,
       appBarTheme: const AppBarTheme(backgroundColor: AppColors.background),
       backgroundColor: AppColors.background,
       primaryColor: AppColors.white,
+      primarySwatch: Colors.green,
       brightness: Brightness.dark,
     );
   }
