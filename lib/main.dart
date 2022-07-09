9e82674909da068ddc332578c9a9c981b8092de2
João Victor Garcia <garcialeite110@hotmail.com>
2022-07-09 15:22:38 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:clean_arch_aula/app_module.dart';
 import 'package:clean_arch_aula/firebase_app_widget.dart';
-import 'package:clean_arch_aula/shared/core/bloc_observer/custom_bloc_observer.dart';
+import 'package:clean_arch_aula/shared/utils/bloc_observer/custom_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
